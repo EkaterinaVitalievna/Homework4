8fcea0b3f40d053295d490c2297d16d475e760e7
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-24 21:52:35 +0300

    Заполнение массива из 8 элементов

---
+++ Task29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// int NewArray = new int [8];
+// FillArray(NewArray);
+// Console.WriteLine(NewArray);
+
+int[] DigitalDial = new int [8];//определили массив из 8 элементов
+Random rand = new Random();
+for (int x=0; x < DigitalDial.Length; x++)
+{
+    DigitalDial[x] = rand.Next(20);
+    Console.WriteLine(DigitalDial[x]);
+}
